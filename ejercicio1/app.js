d3a97da0f4e476ac471343e9dd6e166ab3da278a
EmilioRL <ejruizlinares@gmail.com>
2025-06-25 19:54:02 +0200

    ej1 hecho

---
+++ ejercicio1/app.js
@@ -0,0 +1,24 @@
+
+//1.1 
+const button = document.querySelector(".showme");
+console.log(button);
+
+//1.2
+const pillado = document.querySelector("#pillado");
+console.log(pillado);
+
+//1.3
+const p = document.querySelectorAll("p");
+console.log(p);
+
+//1.4
+const pokemons = document.querySelectorAll(".pokemon");
+console.log(pokemons);
+
+//1.5
+const testME = document.querySelectorAll('[data-function="testMe"]');
+console.log(testME);
+
+//1.6
+const testMe3 = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMe3[2]);
